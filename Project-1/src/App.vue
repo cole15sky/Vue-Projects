afe71fa24a52f5b9fcdec48f969b9346e5c3517d
Sky Blue <dawadiaakash@gmail.com>
2024-08-29 11:50:36 +0545

    Fix: Shopping card render text

---
+++ Project-1/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <ShoppingCart /> 
+  </div>
+</template>
+
+<script>
+import ShoppingCart from './components/ShoppingCart.vue'; 
+
+export default {
+  name: 'App',
+  components: {
+    ShoppingCart
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Project-1/src/components/ShoppingCart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Shopping Cart</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCart",
+};
+</script>
+
+<style>
+</style>
