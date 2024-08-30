9fffa48e11c3fbf95f7500c3ad055a5a3869a05b
Sky Blue <dawadiaakash@gmail.com>
2024-08-30 13:47:21 +0545

    Fix: Api call to render json objects

--- Project-1/src/components/ShoppingCart.vue
+++ Project-1/src/components/ShoppingCart.vue
@@ -2,23 +2,25 @@
 <div>
   <h2>Shopping Cart</h2>
 
-<table >
-  <tr>
-    <th>id</th>
-    <th>price</th>
-    <th>category</th>
-  </tr>
-  <tr>
-    <td> z</td>
-    <td> y</td>
-    <td>x</td>
-  </tr>
-  <tr>
-    <td> a</td>
-    <td> c</td>
-    <td>b</td>
-  </tr>
-</table>
+  <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Price</th>
+          <th>Category</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Use v-for to loop through the fetched data -->
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.id }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+          <td>{{ product.title }}</td>
+        </tr>
+      </tbody>
+    </table>
 
 
 </div>
@@ -28,21 +30,47 @@
 
 
  <script>
-fetch('https://fakestoreapi.com/products')
-.then(res =>{
-return res.json();
-})
+// fetch('https://fakestoreapi.com/products')
+// .then(res =>{
+// return res.json();
+// })
 
-.then(data =>{
-data.forEach(user =>{
-  const markup = `<th>$(user.id)</th>`;
-   console.log(user.description);
-   document.querySelector('tr').insertAdjacentHTML('beforeend',markup)
-});
+// .then(data =>{
+// data.forEach(user =>{
+//   const markup = `<th>$(user.id)</th>`;
+//    console.log(user.description);
+//    document.querySelector('td').insertAdjacentHTML('beforeend',markup)
+// });
 
-})
+// })
 
-.catch(error => console.log(error));
+// .catch(error => console.log(error));
+
+
+import { ref, onMounted } from 'vue';
+
+export default {
+  setup() {
+    const products = ref([]);
+
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        products.value = await response.json();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    onMounted(() => {
+      fetchProducts();
+    });
+
+    return {
+      products
+    };
+  }
+};
 
 </script>
 
