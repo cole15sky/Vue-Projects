3fdac84464d1dbc9631a492bda653e6068345063
Sky Blue <dawadiaakash@gmail.com>
2024-08-30 10:13:30 +0545

    Fetch: Table for json data

--- Project-1/src/App.vue
+++ Project-1/src/App.vue
@@ -6,11 +6,16 @@
 
 <script>
 import ShoppingCart from './components/ShoppingCart.vue'; 
+// import axios from 'axios';
 
 export default {
+
+
+  
   name: 'App',
   components: {
     ShoppingCart
+
   }
 };
 </script>
--- Project-1/src/components/ShoppingCart.vue
+++ Project-1/src/components/ShoppingCart.vue
@@ -1,89 +1,59 @@
 <template>
-  <div class="wrapper">
-    <div class="container">
-      <h3>ShoppingCart.</h3> 
-      <div class="items">
-        <ol>
-        <li>Merlot Red
-          <button> + </button>
-          <button> - </button> 
-          <sub> $ 450</sub>          
+<div>
+  <h2>Shopping Cart</h2>
 
-        </li>
-        <li>Airen White
-
-          <button> + </button>
-          <button> - </button>
-          <sub> $ 350</sub>          
-
-        </li>
-        
-        <li>Antinori
-          
-          <button> + </button>
-          <button> - </button>
-          <sub> $ 650</sub>          
-        </li>
-          
-      </ol>
-      </div>
-      
-      
-
-     <h4> Thanks! Visit Again</h4>
+<table >
+  <tr>
+    <th>id</th>
+    <th>price</th>
+    <th>category</th>
+  </tr>
+  <tr>
+    <td> z</td>
+    <td> y</td>
+    <td>x</td>
+  </tr>
+  <tr>
+    <td> a</td>
+    <td> c</td>
+    <td>b</td>
+  </tr>
+</table>
 
 
-    </div>
 </div>
 </template>
 
-<script>
-export default {
-  name: "ShoppingCart",
-};
+
+
+
+ <script>
+fetch('https://fakestoreapi.com/products')
+.then(res =>{
+return res.json();
+})
+
+.then(data =>{
+data.forEach(user =>{
+  const markup = `<th>$(user.id)</th>`;
+   console.log(user.description);
+   document.querySelector('tr').insertAdjacentHTML('beforeend',markup)
+});
+
+})
+
+.catch(error => console.log(error));
+
 </script>
 
+
+
 <style scoped>
-h3{
-  margin-left:35%;
+table, th, td {
+  border:1px solid black;
 }
-
-.wrapper {
-  display: flex;
-  justify-content: center; 
-  align-items: center; 
-  height: 120vh;
-  background-color: aquamarine;
+table{
+  width: 100%;
 }
-.container{
-  width: 500px;
-  height: 500px;
-  border: 1px solid blue;
-  background-color: turquoise;
-}
-
-.items{
-  background-color:cadetblue;
-  height: 390px;
-  width: 500px;
-}
-
-h4{
-  margin-left: 35%;
-  color: darkgreen;
-}
-
-
-li{
-  font-size: 20px;
-padding: 20px;
-} 
-
-sub{
-  margin-left: 190px;
-}
-
-
-
-
 </style>
+
