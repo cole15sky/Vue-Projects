25e96e7032723c065b64c16de33e6e8611a54ef3
Sky Blue <dawadiaakash@gmail.com>
2024-08-30 15:55:10 +0545

    Fix(issue): Image render on shopping cart

--- Project-1/src/components/ShoppingCart.vue
+++ Project-1/src/components/ShoppingCart.vue
@@ -1,52 +1,35 @@
 <template>
-<div>
-  <h2>Shopping Cart</h2>
-
-  <table>
+  <div>
+    <h2>Shopping Cart</h2>
+    <table>
       <thead>
         <tr>
           <th>ID</th>
           <th>Price</th>
           <th>Category</th>
           <th>Title</th>
+          <th>Image</th>
+          <th>Rating</th>
         </tr>
       </thead>
       <tbody>
-        <!-- Use v-for to loop through the fetched data -->
         <tr v-for="product in products" :key="product.id">
           <td>{{ product.id }}</td>
           <td>{{ product.price }}</td>
           <td>{{ product.category }}</td>
           <td>{{ product.title }}</td>
+          <td><img :src="product.image" alt="Product Image" style="width: 50px; height: 50px;" /></td>
+          <td>{{ product.rating.rate }}</td>
         </tr>
       </tbody>
     </table>
-
-
-</div>
+  </div>
 </template>
 
+<script>
 
 
-
- <script>
-// fetch('https://fakestoreapi.com/products')
-// .then(res =>{
-// return res.json();
-// })
-
-// .then(data =>{
-// data.forEach(user =>{
-//   const markup = `<th>$(user.id)</th>`;
-//    console.log(user.description);
-//    document.querySelector('td').insertAdjacentHTML('beforeend',markup)
-// });
-
-// })
-
-// .catch(error => console.log(error));
-
-
+ 
 import { ref, onMounted } from 'vue';
 
 export default {
@@ -56,6 +39,9 @@ export default {
     const fetchProducts = async () => {
       try {
         const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         products.value = await response.json();
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -67,21 +53,20 @@ export default {
     });
 
     return {
-      products
+      products,
     };
-  }
+  },
 };
-
 </script>
-
-
 
 <style scoped>
 table, th, td {
-  border:1px solid black;
+  border: 1px solid black;
 }
-table{
+table {
   width: 100%;
 }
+img {
+  object-fit: contain;
+}
 </style>
-
