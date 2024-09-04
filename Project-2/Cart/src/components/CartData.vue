036dc2151a0ae0ff1edc1c7339262ce5049ae6bb
Sky Blue <dawadiaakash@gmail.com>
2024-09-04 12:50:56 +0545

    Fetch: Shopping cart icon

---
+++ Project-2/Cart/src/components/CartData.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>This Page stores the {{cart}} history.</h1>
+    </div>
+    
+</template>
+
+
+<script> 
+export default{
+    name:'CartData',
+
+    data(){
+        return{
+            item: cart 
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
